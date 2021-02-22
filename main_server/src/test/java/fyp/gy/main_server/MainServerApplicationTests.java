7f1c9841554a2eea728de52f37f36599469a7586
zzcheah <zzcheah@users.noreply.github.com>
2021-02-22 19:26:42 +0800

    First commit

---
+++ main_server/src/test/java/fyp/gy/main_server/MainServerApplicationTests.java
@@ -0,0 +1,13 @@
+package fyp.gy.main_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
