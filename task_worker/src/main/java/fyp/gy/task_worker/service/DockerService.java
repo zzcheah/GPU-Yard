7c1376d59cfb7482b117dc5cd62c5303a24ee016
zzcheah <zzcheah@users.noreply.github.com>
2021-03-01 21:21:54 +0800

    Task Worker: -> connect to docker daemon -> redefine docker endpoint for different OS (windows with WSL and Linux) -> docker-related functions (check before pull image) ->
    MLaaS logic:
    -> download zip file from main server and unzip it (tested with flat hierarchy)
    -> tested if tensorflow running using GPU
    -> tested running a few tensorflow operations that uses GPU processing power
    -> successfully build a model based on the following tutorial:
    :: https://colab.research.google.com/github/tensorflow/docs/blob/master/site/en/tutorials/quickstart/beginner.ipynb#scrollTo=7FP5258xjs-v
    -> abel to save the model

--- task_worker/src/main/java/fyp/gy/task_worker/service/DockerService.java
+++ task_worker/src/main/java/fyp/gy/task_worker/service/DockerService.java
@@ -3,9 +3,7 @@ package fyp.gy.task_worker.service;
 
 import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.api.command.PullImageResultCallback;
-import com.github.dockerjava.api.model.ExposedPort;
-import com.github.dockerjava.api.model.HostConfig;
-import com.github.dockerjava.api.model.Ports;
+import com.github.dockerjava.api.model.*;
 import com.github.dockerjava.core.DefaultDockerClientConfig;
 import com.github.dockerjava.core.DockerClientConfig;
 import com.github.dockerjava.core.DockerClientImpl;
@@ -15,79 +13,97 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
 @Service
 public class DockerService {
 
     Logger logger = LoggerFactory.getLogger(DockerService.class);
 
+    private static final String OS = System.getProperty("os.name").toLowerCase();
+    public static boolean IS_UNIX = (OS.contains("nix") || OS.contains("nux") || OS.indexOf("aix") > 0);
+    public static boolean IS_WINDOWS = (OS.contains("win"));
+
+
+    DockerClient dockerClient;
+
     public DockerService() {
 
+        String dockerEndpoint = null;
+
+        if (IS_UNIX) dockerEndpoint = "unix:///var/run/docker.sock";
+        else if (IS_WINDOWS) dockerEndpoint = "npipe:////./pipe/docker_engine";
+
         DockerClientConfig config = DefaultDockerClientConfig.createDefaultConfigBuilder()
-                .withDockerHost("npipe:////./pipe/docker_engine").build();
+                .withDockerHost(Objects.requireNonNull(dockerEndpoint)).build();
 
         DockerHttpClient httpClient = new ApacheDockerHttpClient.Builder()
                 .dockerHost(config.getDockerHost())
                 .build();
 
-
-        DockerClient dockerClient = DockerClientImpl.getInstance(config, httpClient);
-        dockerClient.pingCmd().exec();
-
-        String imageName = "tensorflow/tensorflow";
-//        String tag = "latest";
-        String tag = "latest-gpu-py3-jupyter";
-        String containerId = null;
+        dockerClient = DockerClientImpl.getInstance(config, httpClient);
 
         try {
-//            System.out.println("Pulling Image: " + imageName);
-//
-//            dockerClient.pullImageCmd(imageName)
-//                    .withTag(tag)
-//                    .exec(new PullImageResultCallback())
-//                    .awaitCompletion();
-
-            logger.info("Pulled image: " + imageName);
-
-            ExposedPort tcp8888 = ExposedPort.tcp(8888);
-            Ports portBinding = new Ports();
-            portBinding.bind(tcp8888, Ports.Binding.bindPort(8888));
-
-
-            containerId = dockerClient.createContainerCmd(imageName+":"+tag)
-//                    .withName("testTF")
-//                    .withCmd("gpus")
-                    .withHostConfig(new HostConfig().withPortBindings(portBinding).withRuntime("nvidia"))
-                    .withExposedPorts(tcp8888)
-                    .exec().getId();
-
-        } catch (Exception e) {
+            pullImage("tensorflow/tensorflow", "latest-gpu");
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    }
 
-        if (containerId != null) {
-            dockerClient.startContainerCmd(containerId).exec();
+
+    private void pullImage(String image, String tag) throws InterruptedException {
+
+        String imageWithTag = image + ":" + tag;
+
+        final List<Image> images = dockerClient
+                .listImagesCmd()
+                .withImageNameFilter(imageWithTag).exec();
+
+        if (images.isEmpty()) {
+            logger.info("Pulling Image: " + imageWithTag);
+            dockerClient.pullImageCmd(image)
+                    .withTag(tag)
+                    .exec(new PullImageResultCallback())
+                    .awaitCompletion();
+            logger.info("Pulled image: " + imageWithTag);
         }
 
 
-//        List<Container> containers = dockerClient.listContainersCmd()
-//                .withShowSize(true)
-//                .withShowAll(true)
-//                .withStatusFilter(Collections.singleton("exited")).exec();
-//
-//        Container temp = containers.get(1);
-//        System.out.println(temp.getId());
-//        System.out.println(Arrays.toString(temp.getNames()));
-//
-//        System.out.println("running hello world container");
-//
-//        try {
-//            dockerClient.startContainerCmd(temp.getId()).exec();
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
+    }
 
-//        dockerClient.listImagesCmd().exec().get(0).
+    private String runContainerWithImage(String imageWithTag) throws Exception {
+        String id = dockerClient.createContainerCmd(imageWithTag)
+                .withHostConfig(new HostConfig().withRuntime("nvidia"))
+                .exec().getId();
 
+        dockerClient.startContainerCmd(id).exec();
+        return id;
+    }
+
+    public List<Container> listContainers() {
+        return dockerClient.listContainersCmd()
+                .withShowSize(true)
+                .withShowAll(true)
+                .withStatusFilter(Collections.singleton("exited")).exec();
+    }
+
+    private Ports bindPort(int exposedPort, int bindPort) {
+        ExposedPort ep = ExposedPort.tcp(exposedPort);
+        Ports portBinding = new Ports();
+        portBinding.bind(ep, Ports.Binding.bindPort(bindPort));
+        return portBinding;
+
+//            ExposedPort tcp8888 = ExposedPort.tcp(8888);
+//            Ports portBinding = new Ports();
+//            portBinding.bind(tcp8888, Ports.Binding.bindPort(8888));
+//
+//
+//            containerId = dockerClient.createContainerCmd(imageName+":"+tag)
+//                    .withHostConfig(new HostConfig().withPortBindings(portBinding).withRuntime("nvidia"))
+//                    .withExposedPorts(tcp8888)
+//                    .exec().getId();
 
     }
 
