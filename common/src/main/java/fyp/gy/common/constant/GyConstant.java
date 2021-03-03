a48d9de1b0f487ee36f86ac2eb49c0ae318187ee
zzcheah <zzcheah@users.noreply.github.com>
2021-03-04 06:00:16 +0800

    new common packages for better consistency and reduce redundancy

---
+++ common/src/main/java/fyp/gy/common/constant/GyConstant.java
@@ -0,0 +1,10 @@
+package fyp.gy.common.constant;
+
+public class GyConstant {
+
+    public final static String TASKS_COLLECTION = "Requests";
+    public final static String REQUESTS_COLLECTION = "Tasks";
+
+
+
+}
