e9825dce7ae3a0649f1a5873f095feab1a939f8f
zzcheah <zzcheah@users.noreply.github.com>
2021-03-05 22:56:04 +0800

    added eureka module for eureka server setup

---
+++ eureka/src/test/java/fyp/gy/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package fyp.gy.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
