84011d2be73bf76298f6e72e17e49f72911aaac4
zzcheah <zzcheah@users.noreply.github.com>
2021-06-01 02:27:29 +0800

    Task Worker -> added preference node -> added script to verify worker before start polling -> make use of preference to initialize server -> first timer will have to enter worker id -> redirect user to register the worker on web -> added lombok library

--- task_worker/src/main/java/fyp/gy/task_worker/TaskWorkerApplication.java
+++ task_worker/src/main/java/fyp/gy/task_worker/TaskWorkerApplication.java
@@ -1,9 +1,18 @@
 package fyp.gy.task_worker;
 
+import fyp.gy.common.constant.GyConstant;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.URI;
+import java.util.Objects;
+import java.util.Properties;
+import java.util.Scanner;
+import java.util.prefs.Preferences;
 
 @SpringBootApplication(exclude = {
 		MongoAutoConfiguration.class,
@@ -11,8 +20,37 @@ import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 })
 public class TaskWorkerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TaskWorkerApplication.class, args);
+	public static void main(String[] args) throws IOException {
+		Preferences pref = Preferences.userRoot().node(GyConstant.PREFERENCE_NODE);
+		boolean verified =pref.getBoolean("Verified",false);
+		if(verified){
+			SpringApplication.run(TaskWorkerApplication.class, args);
+		} else {
+
+			Scanner sc= new Scanner(System.in);
+			int option = 0;
+			while (option<1||option>2) {
+				System.out.println("Select from the options below:");
+				System.out.println("1. Register Your Worker ");
+				System.out.println("2. Enter Worker ID (after approval)");
+				option = sc.nextInt();
+			}
+			if(option==1) {
+				String rootPath = Objects.requireNonNull(Thread.currentThread().getContextClassLoader().getResource("")).getPath()+"application.properties";
+				Properties appProps = new Properties();
+				appProps.load(new FileInputStream(rootPath));
+				String url = String.format("http://%s:3000/newWorker",appProps.getProperty("mainserver.hostname"));
+				java.awt.Desktop.getDesktop().browse(URI.create(url));
+			} else {
+				System.out.println("Enter Worker Id:");
+				String id = sc.next();
+				pref.put("id",id);
+				SpringApplication.run(TaskWorkerApplication.class, args);
+			}
+
+		}
+
+
 	}
 
 }
--- task_worker/src/main/java/fyp/gy/task_worker/service/TaskService.java
+++ task_worker/src/main/java/fyp/gy/task_worker/service/TaskService.java
@@ -1,5 +1,6 @@
 package fyp.gy.task_worker.service;
 
+import fyp.gy.common.constant.GyConstant;
 import fyp.gy.common.model.Request;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,6 +13,7 @@ import org.springframework.web.client.RestTemplate;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.TimeUnit;
+import java.util.prefs.Preferences;
 
 @Service
 public class TaskService {
@@ -29,62 +31,35 @@ public class TaskService {
     private Timer timer;
     private long poolingInterval;
 
-//    private Thread pollingThread = new Thread(() -> {
-//        while (true) {
-//            logger.info("Checking for job in queue...");
-//            try {
-//                Task task = jobQueue.take();
-//                try {
-//                    logger.info(String.format("Request #%s polled, looking for machine available...", task.getRequestID()));
-//                    processRequest(task.getRequestID());
-//                } catch (Exception e) {
-//                    jobQueue.add(task);
-//                    e.printStackTrace();
-//                }
-//            } catch (InterruptedException e) {
-//                logger.error("fail polling request (QueueService): " + e.getMessage());
-//                e.printStackTrace();
-//            }
-//        }
-//    });
-
     @Autowired
     public TaskService(DockerService docker,
                        @Value("${mainserver.hostname}") String hn,
-                       @Value("${mainserver.port}") String port,
-                       @Value("${spring.application.name}") String workerID
-    ) {
+                       @Value("${mainserver.port}") String port
+    ) throws Exception {
 
         this.docker = docker;
         this.restTemplate = new RestTemplate();
 
         String msURL = String.format("http://%s:%s", hn, port);
-        this.pollURL = msURL + "/request/poll?worker=" + workerID;
 
+        Preferences pref = Preferences.userRoot().node(GyConstant.PREFERENCE_NODE);
+        String id = pref.get("id", null);
+        if(id==null) {
+            pref.putBoolean("Verified", true);
+            throw new Exception("Missing Machine ID");
+        }
+
+        Boolean valid = restTemplate.getForObject(msURL+"/verify?id="+id,Boolean.class);
+        if(valid!=null && valid) {
+            pref.putBoolean("Verified", true);
+        } else {
+            pref.putBoolean("Verified", false);
+            throw new Exception("Invalid Machine ID");
+        }
+
+        this.pollURL = msURL + "/request/poll?worker=" + id;
         resetTimer();
 
-        //<editor-fold desc="Manually run a docker on startup">
-//        logger.info(String.format("MainServer : %s:%s", ip_addr, port));
-//
-//
-//        String downloadURL = "http://192.168.0.121:8080/files/603fe0cd0407ac653c0d0900";
-//
-//        try {
-//            String containerID = dockerClient.createContainerCmd("zz:proto")
-//                    .withHostConfig(hostConfig)
-//                    .withStdinOpen(true)
-//                    .withEnv(
-//                            String.format("inputParam=%s", "ewogICJiYXRjaF9zaXplIjogMzIsCiAgImVwb2NocyI6IDEwLAogICJzZWVkIjogNDIsCiAgInZhbGlkYXRpb25fc3BsaXQiOiAwLjIsCiAgIm1heF9mZWF0dXJlcyI6IDEwMDAwLAogICJzZXF1ZW5jZV9sZW5ndGgiOiAyNTAsCiAgImVtYmVkZGluZ19kaW0iOiAxNiwKICAibG9zcyI6ICJiaW5hcnlfY3Jvc3NlbnRyb3B5IiwKICAib3B0aW1pemVyIjogImFkYW0iCn0"),
-//                            String.format("downloadURL=%s", downloadURL),
-//                            String.format("uploadURL=%s", uploadURL)
-//                    ).exec().getId();
-//
-//            System.out.println("done creating docker");
-//            dockerClient.startContainerCmd(containerID).exec();
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-        //</editor-fold>
     }
 
     private void pollAndRun() {
